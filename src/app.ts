61a04e2c47197ad1685402bb1b16f7e4092c5585
Juan J Malaver <juanmagri@outlook.es>
2025-08-28 13:04:54 -0500

    Fetching data from DB

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,26 @@
 import express from 'express';
+import pkg from 'pg';
+import dotenv from 'dotenv';
 const app = express();
 const port = 3001;
+const { Pool } = pkg;
+dotenv.config();
 
-app.get('/', (req, res) => {
-   res.send('Hello World!');
+const pool = new Pool({
+   user: process.env.PGUSER,
+   host: process.env.PGHOST,
+   port: process.env.PGPORT ? parseInt(process.env.PGPORT, 10) : undefined,
+   database: process.env.PGDATABASE,
+});
+
+app.get('/', async (req, res) => {
+   try {
+      const result = await pool.query('SELECT * FROM todos');
+      res.json(result.rows);
+   } catch (err) {
+      console.error(err);
+      res.status(500).send('Error al obtener los todos');
+   }
 });
 
 app.listen(port, () => {
