d119734c5adbdb0450e15fcfd264ae0e49c0b658
Juan J Malaver <juanmagri@outlook.es>
2025-09-05 09:48:41 -0500

    Search query is now connected to the DB

--- src/routes/search.ts
+++ src/routes/search.ts
@@ -1,14 +1,33 @@
 import { Router } from 'express';
-const router = Router();
-router.get('/', (req, res) => {
-   const terms = req.query.term || 'Search term was not specified';
-   const category = req.query.cat || 'Any';
+import pool from '../db.js';
 
-   res.send(`
-         <h2>Search results:</h2>
-         <p>Term: ${terms}</p>
-         <p>Category: ${category}</p>
-      `);
+const router = Router();
+
+router.get('/', async (req, res) => {
+   try {
+      const terms = req.query.term || 'Search term was not specified';
+      const category = req.query.cat || 'Any';
+      const result = await pool.query(
+         'SELECT * FROM todos WHERE content LIKE $1',
+         [`%${terms}%`]
+      );
+      res.json({
+         search: {
+            terms,
+            category,
+         },
+         results: result.rows,
+      });
+
+      // res.send(`
+      //    <h2>Search results:</h2>
+      //    <p>Term: ${terms}</p>
+      //    <p>Category: ${category}</p>
+      //    `);
+   } catch (err) {
+      console.error(err);
+      res.status(500).send('Internal error');
+   }
 });
 
 export default router;
